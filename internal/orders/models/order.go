f85f3500a60eb8daf9ce11dd6c5890b60e630d27
dmit <exehoua@gmail.com>
2024-10-06 16:14:58 +0300

    change Totalcents to Price for Order struct

--- internal/orders/models/order.go
+++ internal/orders/models/order.go
@@ -3,10 +3,10 @@ package models
 import "github.com/alexandear/truckgo/internal/auth/models"
 
 type Order struct {
-	ID         int         `gorm:"primary_key"`
-	Number     int         `gorm:"unique;not null"` // Додати індекс
-	Status     string      `gorm:"not null"`
-	TotalCents int64       `gorm:"not null;default:0"`
-	UserID     uint        `gorm:"not null"`
-	User       models.User `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:RESTRICT;"`
+	ID     int         `gorm:"primary_key"`
+	Number int         `gorm:"unique;not null"` // Додати індекс
+	Status string      `gorm:"not null"`
+	Price  int64       `gorm:"not null;default:0"` //cents
+	UserID uint        `gorm:"not null"`
+	User   models.User `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:RESTRICT;"`
 }
