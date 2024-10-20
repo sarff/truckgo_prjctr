100a7d5003aba4e84b944c84867da8822edaee8d
sarff <30344665+sarff@users.noreply.github.com>
2024-10-21 01:48:42 +0300

    grpc client from auth to user service. write user to both db with same id (#28)

--- auth/internal/services/auth_tools.go
+++ auth/internal/services/auth_tools.go
@@ -1,11 +1,17 @@
 package services
 
 import (
+	"context"
+	"errors"
 	"fmt"
+	"github.com/alexandear/truckgo/auth/internal/models"
 	"github.com/alexandear/truckgo/shared/logging"
+	userpb "github.com/alexandear/truckgo/user/grpcapi"
 	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/crypto/bcrypt"
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/status"
 	"gorm.io/gorm"
 	"os"
@@ -55,4 +61,39 @@ func (s *AuthServiceServer) checkUserByLogin(login string) error {
 		return fmt.Errorf("failed to query user: %v", err)
 	}
 	return nil
+}
+
+func (s *AuthServiceServer) GrpcConnCreateUser(ctx context.Context, auth *models.Auth) error {
+	conn, err := grpc.NewClient("users:"+os.Getenv("GRPC_PORT_USER"), grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	userClient := userpb.NewUserServiceClient(conn)
+	switch auth.TypeUser {
+	case "driver":
+		req := &userpb.NewDriverRequest{
+			Id:    uint32(auth.ID),
+			Login: auth.Login,
+		}
+		_, err := userClient.NewDriver(ctx, req)
+		if err != nil {
+			return err
+		}
+		return nil
+	case "customer":
+		req := &userpb.NewCustomerRequest{
+			Id:    uint32(auth.ID),
+			Login: auth.Login,
+		}
+		_, err := userClient.NewCustomer(ctx, req)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+
+	err = errors.New("unknown auth type")
+	return err
 }
