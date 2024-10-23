ed00d6988ef690bd9f86e67aaf760807ec514c90
dmit <exehoua@gmail.com>
2024-10-23 22:06:16 +0300

    tests

---
+++ auth/internal/services/auth_tools_test.go
@@ -0,0 +1,41 @@
+package services
+
+import (
+	"golang.org/x/crypto/bcrypt"
+	"testing"
+)
+
+func TestHashPassword(t *testing.T) {
+	password := "strongpassword"
+	hashedPassword, err := HashPassword(password)
+	if err != nil {
+		t.Fatalf("expected no error, got %v", err)
+	}
+
+	if len(hashedPassword) == 0 {
+		t.Errorf("expected hashed password, got empty string")
+	}
+
+	// Test with weak password (less than 8 characters)
+	_, err = HashPassword("short")
+	if err == nil {
+		t.Errorf("expected an error for short password, got nil")
+	}
+}
+
+func TestCheckPassword(t *testing.T) {
+	password := "strongpassword"
+	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+
+	// Correct password
+	err := checkPassword(string(hashedPassword), password)
+	if err != nil {
+		t.Errorf("expected no error, got %v", err)
+	}
+
+	// Incorrect password
+	err = checkPassword(string(hashedPassword), "wrongpassword")
+	if err == nil {
+		t.Errorf("expected an error, got nil")
+	}
+}
---
+++ shared/logging/logger_test.go
@@ -0,0 +1,44 @@
+package logging
+
+import (
+	"github.com/spf13/viper"
+	"os"
+	"testing"
+)
+
+func TestInitLogger(t *testing.T) {
+	tempDir := t.TempDir()
+	viper.Set("log_path", tempDir+"/testlog.log")
+	viper.Set("log_level", "info")
+
+	logger, err := InitLogger("test")
+	if err != nil {
+		t.Fatalf("expected no error, but got: %v", err)
+	}
+	if logger == nil {
+		t.Fatal("expected logger to be initialized, but got nil")
+	}
+
+	if _, err := os.Stat(tempDir + "/test_testlog.log"); os.IsNotExist(err) {
+		t.Fatalf("expected log file to be created, but it was not: %v", err)
+	}
+
+	err = os.Remove(tempDir + "/test_testlog.log")
+	if err != nil {
+		t.Errorf("expected no error, but got: %v", err)
+	}
+}
+
+func TestInitLoggerError(t *testing.T) {
+	tempDir := t.TempDir()
+	viper.Set("log_path", tempDir+"testlog.log")
+	viper.Set("log_level", "info")
+
+	logger, err := InitLogger(tempDir)
+	if err == nil {
+		t.Fatal("expected an error due to invalid path, but got none")
+	}
+	if logger != nil {
+		t.Fatalf("expected logger to be nil on error, but got: %v", logger)
+	}
+}
